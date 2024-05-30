942fc05300770c16f5952a7454a1f612e7dcc56c
otsembo <okumu.otsembo@gmail.com>
2024-05-30 19:58:32 +0300

    added new route

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return '<h1>Welcome to our flask app</h1>'
 
+@app.route('/hello/<string:name>')
+def hello(name):
+    return f'<h1>Buenos Dias {name}</h1>'
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
